7cdc1491d3a2aa9bba9bda9a026f5bc324d26c4c
fabio21777 <fabiosouza21777@gmail.com>
2021-09-13 15:10:06 -0300

    init backend

---
+++ backend/src/test/java/com/fsm/DSCatalog/DsCatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fsm.DSCatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsCatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
